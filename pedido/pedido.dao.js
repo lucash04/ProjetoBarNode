8412b4e8ee646c5536fb4c85692b1c4acf23d8f0
= <=>
2018-12-02 19:40:45 -0200

    Primeira versão funcional

--- pedido/pedido.dao.js
+++ pedido/pedido.dao.js
@@ -4,20 +4,27 @@ const db = require('../db/db');
 module.exports = {
 
     consultar : (callback) => {
+
         // consultar todos os pedidos
         db.connect()
 
-        Pedido.find( (e, res) => {
+        let q = Pedido.find({});
+
+        q.populate("estabelecimento");
+        q.populate("pedido-item");
+        q.sort("-data");
+
+        q.exec( (e, res) => {
             db.disconnect()
             callback(res)
-        })
+        });
     },
 
     registrar : (pedido, callback) => {
 
         // registrar os pedidos, com as informações que virão da 'tela'
         db.connect();
-
+		
         let p = new Pedido(pedido)
         p.data = new Date()
 
--- pedido/pedido.model.js
+++ pedido/pedido.model.js
@@ -5,12 +5,13 @@ const Types = mongoose.Schema.Types;
 var schemaPedido = new Schema({
     data : Date,
     valor : Number,
-    hora_chegada : Date,
+    hora_chegada : String,
     pessoas : Number,
     estabelecimento : {
         type : Types.ObjectId,
         ref : 'Estabelecimento'
-    }
+    },
+    itens : Object
 });
 
 module.exports = mongoose.model("Pedido", schemaPedido);
